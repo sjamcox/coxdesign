5918ffba682c070ce5a04f758d20238fd67d929c
Sam Cox <sam@coxdesign.co>
2019-10-04 15:48:03 -0700

    Added listing query

---
+++ src/components/listing.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const LISTING_QUERY = graphql
+`query BlogPostListing {
+    allMarkdownRemark(limit: 10, sort: {
+        order: DESC,
+        fields: [frontmatter___date]
+    }) {
+        edges {
+          node {
+            excerpt
+            frontmatter {
+              date
+              title
+              slug
+            }
+          }
+        }
+    }
+}`
+
+const Listing = () => (
+  <StaticQuery 
+    query={LISTING_QUERY}
+    render={({allMarkdownRemark}) => (
+        allMarkdownRemark.edges.map(edge => (
+            <article key={edge.node.frontmatter.slug}>
+                <Link to={`/posts${edge.node.frontmatter.slug}`}><h2>{edge.node.frontmatter.title}</h2></Link>
+                <p>{edge.node.frontmatter.date}</p>
+                <p>{edge.node.excerpt}</p>
+                <Link to={`/posts${edge.node.frontmatter.slug}`}>Read More</Link>
+            </article>
+        ))
+    )}
+  />
+)
+
+export default Listing
