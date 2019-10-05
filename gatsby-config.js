7761f647be7f37212d0d7bf7675f8d5760c13ac4
Sam Cox <sam@coxdesign.co>
2019-10-05 12:22:02 -0700

    Added Netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,8 @@ module.exports = {
         gfm: true,
       }
     }
+    `gatsby-plugin-netlify`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
